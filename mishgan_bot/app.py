0bb220e301bc9e118009723373c47296e863f588
mmsolovev <mmsolovevmail@gmail.com>
2022-05-20 22:31:03 +0300

    add: app, func hello world

---
+++ mishgan_bot/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+from os.path import join, dirname
+
+app = Flask(__name__)
+
+
+def get_secret(key):
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    return os.environ.get(key)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run()
